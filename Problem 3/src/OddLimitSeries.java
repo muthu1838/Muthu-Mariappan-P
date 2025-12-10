da87b621a378733f40f4773f5b822618c187db27
muthu1838 <143413443+muthu1838@users.noreply.github.com>
2025-12-10 13:25:36 +0530

    Add files via upload

---
+++ Problem 3/src/OddLimitSeries.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class OddLimitSeries {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            int odd = 2 * i - 1;
+            System.out.print(odd);
+
+            if (i < a) {
+                System.out.print(", ");
+            }
+        }
+
+        sc.close();
+    }
+}
